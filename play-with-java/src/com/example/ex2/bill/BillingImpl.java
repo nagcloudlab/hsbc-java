fb502c09730f52c300744ea347992d168e5bd143
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-07-24 16:36:48 +0530

    Ex

--- play-with-java/src/com/example/ex2/bill/BillingImpl.java
+++ play-with-java/src/com/example/ex2/bill/BillingImpl.java
@@ -2,6 +2,27 @@ package com.example.ex2.bill;
 
 import com.example.ex2.pm.ExcelPriceMatrix;
 
+/*
+
+    design issues
+
+    -> tight-coupling b/w dependent & dependency
+        => maintenance issues i.e not able expant with new features easily
+    -> unit testing not possible
+        => bug fix slow, dev slow
+
+    performance issues
+
+    -> creating dependency obj on each transaction
+        => resource use high, responsive to user..
+
+     why these issues?
+
+     How to fix ?
+
+
+ */
+
 public class BillingImpl {
 
     public double getTotalPrice(String[] cart) {
---
+++ play-with-java/src/com/example/ex2/pm/DBPriceMatrix.java
@@ -0,0 +1,14 @@
+package com.example.ex2.pm;
+
+/**
+ * team : PM team
+ */
+
+public class DBPriceMatrix {
+
+    public double getPrice(String itemCode) {
+        // code to fetch price of item from Database
+        //..
+        return 200.00;
+    }
+}
