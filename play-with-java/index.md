5e1935884ee38f5a068f371b89c27d64b9589e05
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-07-25 14:35:31 +0530

    inheritance

--- play-with-java/index.md
+++ play-with-java/index.md
@@ -2,3 +2,4 @@
 
 ex1 : class & objects
 ex2 : object's concepts & principles
+ex3 : inheritance
---
+++ play-with-java/src/com/example/ex3/Ops.java
@@ -0,0 +1,24 @@
+package com.example.ex3;
+
+public class Ops extends Employee {
+
+    // Fields
+    private int id;
+    private String name;
+    private double salary;
+    private String opsSkills;
+
+    // Constructor
+    public Ops(int id, String name, double salary, String opsSkills) {
+//        this.id = id;
+//        this.name = name;
+//        this.salary = salary;
+        super(id, name, salary);
+        this.opsSkills = opsSkills;
+    }
+
+    public String getOpsSkills() {
+        return opsSkills;
+    }
+
+}
