474a81a7aac9e7909d231cda9a7b51ce8f2f8009
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-07-25 14:04:24 +0530

    basic LinkedList

---
+++ play-with-java/src/com/example/dsa/Application.java
@@ -0,0 +1,16 @@
+package com.example.dsa;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        LinkedList list = new LinkedList();
+        list.add(10);
+        list.add(20);
+
+        System.out.println(list.get(0)); // 10
+        System.out.println(list.get(1)); // 20
+
+    }
+
+}
---
+++ play-with-java/src/com/example/dsa/LinkedList.java
@@ -0,0 +1,44 @@
+package com.example.dsa;
+
+public class LinkedList {
+
+    private Node head;
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public int get(int index) {
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+
+    private class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+}
+
