613e6ed2faf1ab60ea22ba2fdf41e9c196d46df2
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-07-26 16:49:19 +0530

    Stringbuffer

---
+++ play-with-java/src/com/example/ex7/Application.java
@@ -0,0 +1,36 @@
+package com.example.ex7;
+
+import com.example.ex7.p1.A;
+import com.example.ex7.p1.B;
+import com.example.ex7.p1.C;
+import com.example.ex7.p2.D;
+import com.example.ex7.p2.E;
+
+public class Application {
+    public static void main(String[] args) {
+
+        A a = new A();
+        a.aInstanceMethod();
+
+        System.out.println();
+
+        B b = new B();
+        b.bInstanceMethod();
+
+        System.out.println();
+
+        C c = new C();
+        c.cInstanceMethod();
+
+        System.out.println();
+
+        D d = new D();
+        d.dInstanceMethod();
+
+        System.out.println();
+
+        E e = new E();
+        e.eInstanceMethod();
+
+    }
+}
