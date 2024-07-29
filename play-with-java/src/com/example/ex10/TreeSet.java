f8f8e9c30823b058ecfd753ba28d9161c1b1f606
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-07-29 14:43:41 +0530

    Exercise

--- play-with-java/src/com/example/ex10/TreeSet.java
+++
@@ -1,7 +0,0 @@
-package com.example.ex10;
-
-public class TreeSet {
-    class Node{ }
-}
-
-
--- play-with-java/src/com/example/pattern/Ex.java
+++ play-with-java/src/com/example/pattern/Ex.java
@@ -1,92 +1,35 @@
 package com.example.pattern;
 
-import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
+/*
 
-interface DoorListener {
-    void on();
+    design issues
+    ---------------
 
-    void off();
-}
+    -> mix of concerns / code tangling / code coupling
+    -> code scattering / duplicate code
 
-class Light implements DoorListener {
-    public void on() {
-        System.out.println("Light is on");
+ */
+
+class Greeting {
+    public void hello() {
+        System.out.println("Hello");
+        System.out.println("😀");
     }
 
-    public void off() {
-        System.out.println("Light is off");
-    }
-}
-
-class Fan implements DoorListener {
-    public void on() {
-        System.out.println("Fan is on");
-    }
-
-    public void off() {
-        System.out.println("Fan is off");
+    public void hi() {
+        System.out.println("Hi");
+        System.out.println("😀");
     }
 }
 
-class Alarm implements DoorListener {
-    public void on() {
-        System.out.println("Alarm is on");
-    }
-
-    public void off() {
-        System.out.println("Alarm is off");
-    }
-}
-
-class Door {
-
-    private ArrayList<DoorListener> listeners = new ArrayList<>();
-
-    public void addListener(DoorListener listener) {
-        listeners.add(listener);
-    }
-
-    public void removeListener(DoorListener listener) {
-        listeners.remove(listener);
-    }
-
-    public void open() {
-        System.out.println("Door is open");
-        for (DoorListener listener : listeners) {
-            listener.on();
-        }
-    }
-
-    public void close() {
-        System.out.println("Door is closed");
-        for (DoorListener listener : listeners) {
-            listener.off();
-        }
-    }
-}
-
-
-// pattern : Observer/Listener pattern
 
 public class Ex {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
+        Greeting greeting = new Greeting();
+        greeting.hello();
+        greeting.hi();
 
-        Door door = new Door();
-
-        Light light = new Light();
-        Fan fan = new Fan();
-        Alarm alarm = new Alarm();
-
-        door.addListener(light);
-        door.addListener(fan);
-        door.addListener(alarm);
-
-        TimeUnit.SECONDS.sleep(5);
-        door.open();
-        TimeUnit.SECONDS.sleep(5);
-        door.close();
 
     }
 }
