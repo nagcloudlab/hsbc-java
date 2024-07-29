2dae9fe3841fe21e6731545a3e62c679beb09e39
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-07-29 16:00:07 +0530

    builder pattern

--- play-with-java/src/com/example/pattern/Builder.java
+++ play-with-java/src/com/example/pattern/Builder.java
@@ -5,12 +5,33 @@ class Customer {
     private String address;
     private String phone;
     private String email;
-    public Customer(String name, String address, String phone, String email) {
+
+    Customer(String name){
+        //this.name = name;
+        this(name, "", "", "");
+    }
+
+    Customer(String name, String address){
+//        this.name = name;
+//        this.address = address;
+        this(name, address, "", "");
+    }
+
+
+    Customer(String name, String address, String phone){
+//        this.name = name;
+//        this.address = address;
+//        this.phone = phone;
+        this(name, address, phone, "");
+    }
+
+    Customer(String name, String address, String phone, String email){
         this.name = name;
         this.address = address;
         this.phone = phone;
         this.email = email;
     }
+
 }
 
 
@@ -18,16 +39,18 @@ public class Builder {
 
     public static void main(String[] args) {
 
-        Customer c1 = new Customer("John", "123 Main St", "123-456-7890", "john@mail.com");
 
-        /*
+        // without builder
 
-            Customer c1=new Customer.Builder()
-                .name("John")
-                .phone("123-456-7890")
-                .address("123 Main St")
+        Customer customer = new Customer("John", "123 Main St", "123-456-7890", "j@mail.com");
 
-         */
+        // with builder
+//
+//        Customer customer1 = new Customer("John")
+//                .address("123 Main St")
+//                .phone("123-456-7890")
+//                .email("j@mail.com")
+//                .build();
 
     }
 
