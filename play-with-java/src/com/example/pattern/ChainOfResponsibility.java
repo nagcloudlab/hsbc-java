5cc6b018c1d275950327b24cbc7b26f9d95eb9be
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-07-30 14:11:19 +0530

    chain of responsibility

--- play-with-java/src/com/example/pattern/ChainOfResponsibility.java
+++ play-with-java/src/com/example/pattern/ChainOfResponsibility.java
@@ -1,26 +1,14 @@
 package com.example.pattern;
 
 // Chain of Responsibility Pattern
-/*
-
-    Need ATM box,
-    where we can withdraw money in multiple of 100, 500, 2000.
-    If we try to withdraw 1000, it should give 500 + 500.
-    If we try to withdraw 1500, it should give 500 + 500 + 500.
-    If we try to withdraw 2000, it should give 2000.
-    If we try to withdraw 2500, it should give 2000 + 500.
-    If we try to withdraw 3000, it should give 2000 + 1000.
-    If we try to withdraw 3500, it should give 2000 + 1000 + 500.
-    If we try to withdraw 4000, it should give 2000 + 2000.
-    If we try to withdraw 4500, it should give 2000 + 2000 + 500.
-
- */
+// ATM Machine Example
 
 // Handler
 abstract class Handler {
     protected Handler nextHandler;
-    public void setNextHandler(Handler nextHandler) {
+    public Handler setNextHandler(Handler nextHandler) {
         this.nextHandler = nextHandler;
+        return this.nextHandler;
     }
     public abstract void handleRequest(int amount);
 }
@@ -54,10 +42,10 @@ public class ChainOfResponsibility {
         Handler handler100 = new ATMHandler(100);
         Handler handler500 = new ATMHandler(500);
 
-        handler100.setNextHandler(handler50);
-        handler500.setNextHandler(handler100);
+        handler500.setNextHandler(handler50).setNextHandler(handler100);
 
-        handler500.handleRequest(1625);
+        handler500.handleRequest(3650);
+
 
 
     }
