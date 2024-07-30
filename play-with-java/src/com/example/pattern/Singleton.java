bebdf07cbdb86d4d3e7e21cd10c4033dc8d0c47e
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-07-30 11:38:05 +0530

    Singleton pattern

---
+++ play-with-java/src/com/example/pattern/Singleton.java
@@ -0,0 +1,46 @@
+package com.example.pattern;
+
+// singleton pattern
+
+// why singleton?
+// 1. To ensure that a class has only one instance
+// 2. To provide a global point of access to the object
+// 3. To provide a way to access the object without instantiating it
+
+class DeviceDriver {
+    private static DeviceDriver instance = null;
+    private DeviceDriver() {
+        // Initialization
+    }
+    public static DeviceDriver getInstance() {
+        if (instance == null) {
+            instance = new DeviceDriver();
+        }
+        return instance;
+    }
+
+    public void open() {
+        System.out.println("DeviceDriver open");
+    }
+
+    public void close() {
+        System.out.println("DeviceDriver close");
+    }
+}
+
+
+public class Singleton {
+    public static void main(String[] args) {
+
+        // DeviceDriver driver = new DeviceDriver(); // Error: The constructor DeviceDriver() is not visible
+        // ----- module-1 -----
+        DeviceDriver driver = DeviceDriver.getInstance();
+        driver.open();
+
+
+        // ----- module-2 -----
+        DeviceDriver driver2 = DeviceDriver.getInstance();
+        driver2.close();
+
+    }
+}
