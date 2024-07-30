2626acb4c28798ee150b9bc0a834679f38abc0c4
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-07-30 16:26:15 +0530

    Strategy pattern

--- play-with-java/src/com/example/pattern/Ex.java
+++ play-with-java/src/com/example/pattern/Ex.java
@@ -1,10 +1,14 @@
 package com.example.pattern;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Music-Player with Playlist
+ * Strategy Pattern
+ * <p>
+ * e.g PlayList , getNextTrack() method can have different strategies
  */
+
 
 class Track {
     private String title;
@@ -24,16 +28,60 @@ class Track {
     }
 }
 
+
+// Strategy
+interface PlayListStrategy {
+    Track getNextTrack(List<Track> tracks);
+}
+
+// Concrete Strategy
+class RandomPlayListStrategy implements PlayListStrategy {
+    @Override
+    public Track getNextTrack(List<Track> tracks) {
+        int index = (int) (Math.random() * tracks.size());
+        return tracks.remove(index);
+    }
+}
+
+// Concrete Strategy
+class RepeatPlayListStrategy implements PlayListStrategy {
+    @Override
+    public Track getNextTrack(List<Track> tracks) {
+        return tracks.get(0);
+    }
+}
+
+// Concrete Strategy
+class ShortestDurationPlayListStrategy implements PlayListStrategy {
+    @Override
+    public Track getNextTrack(List<Track> tracks) {
+        Track shortestTrack = tracks.get(0);
+        for (Track track : tracks) {
+            if (track.getDuration() < shortestTrack.getDuration()) {
+                shortestTrack = track;
+            }
+        }
+        tracks.remove(shortestTrack);
+        return shortestTrack;
+    }
+}
+
 // Open-Closed Principle
 class PlayList {
+
     private ArrayList<Track> tracks = new ArrayList<>();
+    private PlayListStrategy playListStrategy;
+
+    public void setPlayListStrategy(PlayListStrategy playListStrategy) {
+        this.playListStrategy = playListStrategy;
+    }
 
     public void addTrack(Track track) {
         tracks.add(track);
     }
 
     public Track getNextTrack() {
-        return tracks.remove(0);
+        return playListStrategy.getNextTrack(tracks);
     }
 }
 
@@ -49,11 +97,17 @@ public class Ex {
         playList.addTrack(new Track("Track 4", 210));
         playList.addTrack(new Track("Track 5", 240));
 
+        // set any strategy here
+        // e,g -> random, repeat, shuffle, sdf, ldf, etc.
+        //playList.setPlayListStrategy(new RandomPlayListStrategy());
+        //playList.setPlayListStrategy(new RepeatPlayListStrategy());
+        playList.setPlayListStrategy(new ShortestDurationPlayListStrategy());
+
         // Play the tracks
         int i = 0;
         while (i < 5) {
             Track track = playList.getNextTrack();
-            System.out.println("Playing: " + track.getTitle());
+            System.out.println("Playing: " + track.getTitle() + " - " + track.getDuration());
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
