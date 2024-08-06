7de8e0715b9a335b638a76babd68e1b51366976b
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-08-06 14:55:33 +0530

    Streams Api

--- play-with-java/src/com/example/streams/Ex1.java
+++ play-with-java/src/com/example/streams/Ex1.java
@@ -2,7 +2,10 @@ package com.example.streams;
 
 import com.example.streams.model.Dish;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Ex1 {
 
@@ -22,9 +25,97 @@ public class Ex1 {
         );
 
 
-        //Req-: Find Low calorie dishes names in asc order ( < 400 calories)
+        // Data process pipeline
+        // input --> process -> output
+
+        // way-1 : before 1.8
+
+        System.out.println(
+                findLowCaloricDishesV1(menu)
+        );
+
+        // way-2 : 1.8 onwards with lambda
+        System.out.println(
+                findLowCaloricDishesV2(menu)
+        );
+
+        // way-2 : 1.8 onwards with lambda + streams
+        System.out.println(
+                findLowCaloricDishesV2(menu)
+        );
+
+    }
 
 
+    // Imperative style
+    private static List<String> findLowCaloricDishesV3(List<Dish> menu) {
+
+//        return menu.stream()
+//                .filter(dish -> dish.getCalories() < 400)
+//                .sorted((d1, d2) -> d1.getCalories() - d2.getCalories())
+//                .map(d -> d.getName())
+//                .collect(Collectors.toList());
+
+//
+//        return menu.stream()
+//                .filter(dish -> dish.getCalories() < 400)
+//                .sorted(Comparator.comparing(Dish::getCalories))
+//                .map(Dish::getName)
+//                .collect(Collectors.toList());
+
+
+        //
+        return menu.stream()
+                .parallel()
+                .filter(dish -> dish.getCalories() < 400)
+                .sorted(Comparator.comparing(Dish::getCalories))
+                .map(Dish::getName)
+                .collect(Collectors.toList());
+
+
+    }
+
+
+    private static List<String> findLowCaloricDishesV2(List<Dish> menu) {
+        // step-1 : Find Low Caloric Dishes
+        List<Dish> lowCaloricDishes = new ArrayList<>();
+        for (Dish dish : menu) {
+            if (dish.getCalories() < 400) {
+                lowCaloricDishes.add(dish);
+            }
+        }
+        // step-2 : sort by calories
+        lowCaloricDishes.sort((d1, d2) -> d1.getCalories() - d2.getCalories());
+        // step-3 : get dish names
+        List<String> dishNames = new ArrayList<>();
+        for (Dish dish : lowCaloricDishes) {
+            dishNames.add(dish.getName());
+        }
+
+        return dishNames;
+    }
+
+    private static List<String> findLowCaloricDishesV1(List<Dish> menu) {
+        // step-1 : Find Low Caloric Dishes
+        List<Dish> lowCaloricDishes = new ArrayList<>();
+        for (Dish dish : menu) {
+            if (dish.getCalories() < 400) {
+                lowCaloricDishes.add(dish);
+            }
+        }
+        // step-2 : sort by calories
+        lowCaloricDishes.sort(new Comparator<Dish>() {
+            @Override
+            public int compare(Dish o1, Dish o2) {
+                return o1.getCalories() - o2.getCalories();
+            }
+        });
+        // step-3 : get dish names
+        List<String> dishNames = new ArrayList<>();
+        for (Dish dish : lowCaloricDishes) {
+            dishNames.add(dish.getName());
+        }
+        return dishNames;
     }
 
 }
