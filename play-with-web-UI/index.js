a03603ecff34102dc413d5831ffb8e85b453ea80
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-08-08 15:16:20 +0530

    Timer Api

--- play-with-web-UI/index.js
+++ play-with-web-UI/index.js
@@ -20,3 +20,38 @@ hidetBtn.addEventListener("click", (e) => {
 showBtn.addEventListener("click", (e) => {
   card.style.display = "block";
 });
+
+//-----------------------------------------------
+// using DOM Api + Timer Api
+//-----------------------------------------------
+
+const images = [
+  "./images/1.png",
+  "./images/2.webp",
+  "./images/3.jpg",
+  "./images/4.webp",
+  "./images/5.jpeg",
+];
+
+const poImgEle = document.querySelector("#po-img");
+const startBtn = document.querySelector("#start-btn");
+const stopBtn = document.querySelector("#stop-btn");
+
+stopBtn.disabled = true;
+let intervalId = null;
+startBtn.addEventListener("click", (e) => {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  intervalId = setInterval(() => {
+    console.log("tick");
+    const randomIndex = Math.floor(Math.random() * images.length);
+    poImgEle.src = images[randomIndex];
+  }, 1000);
+});
+
+stopBtn.addEventListener("click", (e) => {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  poImgEle.src = "./images/1.png";
+});
