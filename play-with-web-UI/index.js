f010bbbb948c47d1279ae8f05ce9c12d9b32d084
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-08-08 16:14:08 +0530

    AJAX

--- play-with-web-UI/index.js
+++ play-with-web-UI/index.js
@@ -1,3 +1,84 @@
+//--------------------------------------------------
+// using DOM Api + XHR (XMLHttpRequest) / Fetch Api
+//-------------------------------------------------
+
+const top5TodosBtn = document.querySelector("#top5-todos-btn");
+const todosTableBody = document.querySelector("#todos");
+
+top5TodosBtn.addEventListener("click", (e) => {
+  //loadTodosV1();
+  // loadTodosV2();
+  loadTodosV3();
+});
+
+// Using XHR Api
+function loadTodosV1() {
+  const url = "https://jsonplaceholder.typicode.com/todos?_limit=5";
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true); // Async
+  xhr.send();
+
+  // xhr readystate
+  // 0: request not initialized
+  // 1: server connection established
+  // 2: request received
+  // 3: processing request
+  // 4: request finished and response is ready
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const jsonText = xhr.responseText;
+      const todos = JSON.parse(jsonText);
+      const todoRows = todos.map((todo) => {
+        return `
+      <tr>
+        <td>${todo.id}</td>
+        <td>${todo.title}</td>
+        <td>${todo.completed}</td>
+      </tr> 
+    `;
+      });
+      todosTableBody.innerHTML = todoRows.join("");
+    }
+  };
+}
+
+// Using Fetch Api
+function loadTodosV2() {
+  const url = "https://jsonplaceholder.typicode.com/todos?_limit=5";
+  fetch(url)
+    .then((response) => response.json())
+    .then((todos) => {
+      const todoRows = todos.map((todo) => {
+        return `
+      <tr>
+        <td>${todo.id}</td>
+        <td>${todo.title}</td>
+        <td>${todo.completed}</td>
+      </tr> 
+    `;
+      });
+      todosTableBody.innerHTML = todoRows.join("");
+    });
+}
+
+// Using Fetch Api + Async/Await
+async function loadTodosV3() {
+  const url = "https://jsonplaceholder.typicode.com/todos?_limit=5";
+  const response = await fetch(url);
+  const todos = await response.json();
+  const todoRows = todos.map((todo) => {
+    return `
+      <tr>
+        <td>${todo.id}</td>
+        <td>${todo.title}</td>
+        <td>${todo.completed}</td>
+      </tr> 
+    `;
+  });
+  todosTableBody.innerHTML = todoRows.join("");
+}
+
 //--------------------------
 // using DOM Api
 //--------------------------
